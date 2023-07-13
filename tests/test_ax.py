d51ad613d41b73f8c923993e9da964b8b560ffdb
sgbaird <sterling.baird@utah.edu>
2023-07-13 01:02:27 -0400

    Update test_ax.py

--- tests/test_ax.py
+++ tests/test_ax.py
@@ -1,7 +1,1 @@
-# from honegumi.ax.utils.constants import template_dir
-# from os import path
-# from doctest import testmod
-
-# testmod(, verbose=True)
-
-# 1 + 1
+from honegumi.ax.utils import constants  # noqa
