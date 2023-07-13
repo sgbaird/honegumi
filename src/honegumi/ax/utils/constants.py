f835f7583dfe93c3539e5e21621cdffd110db7c5
sgbaird <sterling.baird@utah.edu>
2023-07-13 00:32:46 -0400

    Create constants.py

---
+++ src/honegumi/ax/utils/constants.py
@@ -0,0 +1,7 @@
+from os import path
+
+objective_opt_name = "objective"
+model_opt_name = "model"
+use_custom_gen_opt_name = "use_custom_gen"
+template_dir = path.join("src", "honegumi", "ax", "templates")
+gen_template_dir = path.join("docs", "generated_scripts", "ax")
