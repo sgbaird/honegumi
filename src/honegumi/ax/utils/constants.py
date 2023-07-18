7a1ba6ed19dd8aecddea84395a93c07147d902a5
sgbaird <sterling.baird@utah.edu>
2023-07-18 00:11:21 -0400

    test template dir

--- src/honegumi/ax/utils/constants.py
+++ src/honegumi/ax/utils/constants.py
@@ -5,3 +5,4 @@ model_opt_name = "model"
 use_custom_gen_opt_name = "use_custom_gen"
 template_dir = path.join("src", "honegumi", "ax", "templates")
 gen_template_dir = path.join("docs", "generated_scripts", "ax")
+test_template_dir = path.join("tests", "generated_scripts", "ax")
