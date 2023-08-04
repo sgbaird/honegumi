56b7f5e295b8575a176dac1660afa684df28fcde
sgbaird <sterling.baird@utah.edu>
2023-08-04 10:28:46 -0400

    rendered_template_stem fns

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,6 +1,11 @@
 import pytest
 
-from honegumi.core.skeleton import fib, main
+from honegumi.core.skeleton import (
+    fib,
+    get_rendered_template_stem,
+    main,
+    unpack_rendered_template_stem,
+)
 
 __author__ = "sgbaird"
 __copyright__ = "sgbaird"
@@ -14,6 +19,64 @@ def test_fib():
     assert fib(7) == 13
     with pytest.raises(AssertionError):
         fib(-10)
+
+
+def test_get_rendered_template_stem():
+    # Test case 1: Single option
+    data = {"option1": "value1"}
+    option_names = ["option1"]
+    expected_output = "option1-value1"
+    assert get_rendered_template_stem(data, option_names) == expected_output
+
+    # Test case 2: Multiple options
+    data = {"option1": "value1", "option2": "value2", "option3": "value3"}
+    option_names = ["option1", "option2", "option3"]
+    expected_output = "option1-value1__option2-value2__option3-value3"
+    assert get_rendered_template_stem(data, option_names) == expected_output
+
+    # Test case 3: Boolean option
+    data = {"option1": True}
+    option_names = ["option1"]
+    expected_output = "option1-True"
+    assert get_rendered_template_stem(data, option_names) == expected_output
+
+    # Test case 4: Integer option
+    data = {"option1": 123}
+    option_names = ["option1"]
+    expected_output = "option1-123"
+    assert get_rendered_template_stem(data, option_names) == expected_output
+
+    # Test case 5: Empty option list
+    data = {"option1": "value1"}
+    option_names = []
+    expected_output = ""
+    assert get_rendered_template_stem(data, option_names) == expected_output
+
+
+def test_unpack_rendered_template_stem():
+    # Test case 1: Single option
+    rendered_template_stem = "option1-value1"
+    expected_output = {"option1": ["value1"]}
+    assert unpack_rendered_template_stem(rendered_template_stem) == expected_output
+
+    # Test case 2: Multiple options
+    rendered_template_stem = "option1-value1__option2-value2__option3-value3"
+    expected_output = {
+        "option1": ["value1"],
+        "option2": ["value2"],
+        "option3": ["value3"],
+    }
+    assert unpack_rendered_template_stem(rendered_template_stem) == expected_output
+
+    # Test case 3: Multiple values for the same option
+    rendered_template_stem = "option1-value1__option1-value2"
+    expected_output = {"option1": ["value1", "value2"]}
+    assert unpack_rendered_template_stem(rendered_template_stem) == expected_output
+
+    # Test case 4: Empty input
+    rendered_template_stem = ""
+    expected_output = {}
+    assert unpack_rendered_template_stem(rendered_template_stem) == expected_output
 
 
 def test_main(capsys):
