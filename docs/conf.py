7d5c461bafce5aa3a094844be04cd3375197a9ed
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-02-05 14:12:34 -0500

    try dark mode

--- docs/conf.py
+++ docs/conf.py
@@ -75,6 +75,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
+    "sphinx_rtd_dark_mode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
