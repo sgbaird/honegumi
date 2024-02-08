d6a70abad932cb939f924314d0e400ec98b4dd98
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-02-08 13:47:25 -0500

    rename "NAME" to "KEY"

--- src/honegumi/ax/utils/constants.py
+++ src/honegumi/ax/utils/constants.py
@@ -1,22 +1,24 @@
 from os import path
 
-OBJECTIVE_OPT_NAME = "objective"
-MODEL_OPT_NAME = "model"
-CUSTOM_GEN_NAME = "custom_gen"
-EXISTING_DATA_NAME = "existing_data"
+OBJECTIVE_OPT_KEY = "objective"
+MODEL_OPT_KEY = "model"
+CUSTOM_GEN_KEY = "custom_gen"
+EXISTING_DATA_KEY = "existing_data"
 # USE_CONSTRAINTS_NAME = "use_constraint"
-SUM_CONSTRAINT_NAME = "sum_constraint"
-ORDER_CONSTRAINT_NAME = "order_constraint"
-LINEAR_CONSTRAINT_NAME = "linear_constraint"
-COMPOSITIONAL_CONSTRAINT_NAME = "composition_constraint"
-CATEGORICAL_NAME = "categorical"
-USE_CUSTOM_THRESHOLD_NAME = "custom_threshold"
-PREDEFINED_CANDIDATES_NAME = "predef_candidates"
-FEATURIZATION_NAME = "featurize"
-CONTEXTUAL_VAR_NAME = "context"
-FIDELITY_OPT_NAME = "fidelity"
-TASK_OPT_NAME = "task"
-SYNCHRONY_OPT_NAME = "synchrony"  # single, batch, asynchronous
+SUM_CONSTRAINT_KEY = "sum_constraint"
+ORDER_CONSTRAINT_KEY = "order_constraint"
+LINEAR_CONSTRAINT_KEY = "linear_constraint"
+COMPOSITIONAL_CONSTRAINT_KEY = "composition_constraint"
+CATEGORICAL_KEY = "categorical"
+CUSTOM_THRESHOLD_KEY = "custom_threshold"
+PREDEFINED_CANDIDATES_KEY = "predef_candidates"
+FEATURIZATION_KEY = "featurize"
+CONTEXTUAL_VAR_KEY = "context"
+FIDELITY_OPT_KEY = "fidelity"
+TASK_OPT_KEY = "task"
+SYNCHRONY_OPT_KEY = "synchrony"  # single, batch, asynchronous
+DUMMY_KEY = "dummy"
+MODEL_KWARGS_KEY = "model_kwargs"
 
 TEMPLATE_DIR = path.join("src", "honegumi", "ax")
 CORE_TEMPLATE_DIR = path.join("src", "honegumi", "core")
@@ -24,4 +26,3 @@ GEN_SCRIPT_DIR = path.join("docs", "generated_scripts", "ax")
 GEN_NOTEBOOK_DIR = path.join("docs", "generated_notebooks", "ax")
 TEST_TEMPLATE_DIR = path.join("tests", "generated_scripts", "ax")
 DOC_DIR = "docs"
-MODEL_KWARGS_NAME = "model_kwargs"
