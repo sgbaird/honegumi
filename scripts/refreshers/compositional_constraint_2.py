54723cde320c9c8636692e2f6a7ced40b0a19ba8
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-02-09 17:42:16 -0500

    Update compositional_constraint_2.py

--- scripts/refreshers/compositional_constraint_2.py
+++ scripts/refreshers/compositional_constraint_2.py
@@ -19,7 +19,7 @@ def branin3(x1, x2, x3):
 
 total = 10.0
 
-ax_client = AxClient()
+ax_client = AxClient(random_seed=42)
 ax_client.create_experiment(
     parameters=[
         {"name": "x1", "type": "range", "bounds": [0.0, total]},
@@ -33,20 +33,31 @@ ax_client.create_experiment(
     ],
 )
 
-for _ in range(10):
-    parameters, trial_index = ax_client.get_next_trial()
+for _ in range(21):
+    parameterization, trial_index = ax_client.get_next_trial()
 
     # # calculate x3 based on compositional constraint, x1 + x2 + x3 == 1
-    x1 = parameters["x1"]
-    x2 = parameters["x2"]
+    x1 = parameterization["x1"]
+    x2 = parameterization["x2"]
+
     x3 = total - (x1 + x2)
 
-    results = branin3(
-        parameters["x1"],
-        parameters["x2"],
-        total - (parameters["x1"] + parameters["x2"]),  # x3
-    )
+    results = branin3(x1, x2, x3)
+
     ax_client.complete_trial(trial_index=trial_index, raw_data=results)
+
+    # # If x1 + x2 is slightly greater than total within a certain tolerance, adjust it
+    # if x1 + x2 > total:
+    #     excess = (x1 + x2) - total
+    #     # Adjust x1 and x2 proportionally to their current values
+    #     x1 -= excess * (x1 / (x1 + x2))
+    #     x2 -= excess * (x2 / (x1 + x2))
+
+    #     x3 = total - (x1 + x2)
+    #     results = branin3(x1, x2, x3)
+
+    #     parameterization, trial_index = ax_client.attach_trial({"x1": x1, "x2": x2})
+    #     ax_client.complete_trial(trial_index=trial_index, raw_data=results)
 
 
 best_parameters, metrics = ax_client.get_best_parameters()
