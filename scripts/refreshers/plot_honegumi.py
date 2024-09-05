ccd5ebfc61db08f5f4a04d6ab459a7f0a030fd88
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-09-04 21:12:48 -0400

    precommit

--- scripts/refreshers/plot_honegumi.py
+++ scripts/refreshers/plot_honegumi.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# fmt: off
+ax_client = None
+batch_size = 2
+
 # single objective, single observation -------------------------------------------------
 
 objectives = ax_client.objective_names
 df = ax_client.get_trials_data_frame()[objectives]
 
-trace = np.minimum.accumulate(df) # change if maximizing
+trace = np.minimum.accumulate(df)  # change if maximizing
 
 fig, ax = plt.subplots(figsize=(6, 4), dpi=150)
 ax.scatter(df.index, df, ec="k", fc="none", label="Observed")
@@ -24,7 +26,7 @@ objectives = ax_client.objective_names
 df = ax_client.get_trials_data_frame()[objectives]
 df.index = df.index // batch_size
 
-trace = np.minimum.accumulate(df.groupby(df.index).min()) # change if maximizing
+trace = np.minimum.accumulate(df.groupby(df.index).min())  # change if maximizing
 
 fig, ax = plt.subplots(figsize=(6, 4), dpi=150)
 ax.scatter(df.index, df, ec="k", fc="none", label="Observed")
@@ -44,7 +46,13 @@ pareto = pd.DataFrame([p[1][0] for p in pareto.values()]).sort_values(objectives
 
 fig, ax = plt.subplots(figsize=(6, 4), dpi=200)
 ax.scatter(df[objectives[0]], df[objectives[1]], fc="None", ec="k", label="Observed")
-ax.plot(pareto[objectives[0]], pareto[objectives[1]], color="#0033FF", lw=2, label="Pareto Front")
+ax.plot(
+    pareto[objectives[0]],
+    pareto[objectives[1]],
+    color="#0033FF",
+    lw=2,
+    label="Pareto Front",
+)
 ax.set_xlabel(objectives[0])
 ax.set_ylabel(objectives[1])
 ax.legend()
@@ -59,7 +67,7 @@ objectives.extend(["task"])
 df = ax_client.get_trials_data_frame()[objectives]
 df = df[df.task == task].drop(columns=["task"])
 
-trace = np.minimum.accumulate(df) # change if maximizing
+trace = np.minimum.accumulate(df)  # change if maximizing
 
 fig, ax = plt.subplots(figsize=(6, 4), dpi=150)
 ax.scatter(df.index, df, ec="k", fc="none", label="Observed")
@@ -80,7 +88,7 @@ df = ax_client.get_trials_data_frame()[objectives]
 df = df[df.task == task].drop(columns=["task"])
 df.index = df.index // batch_size
 
-trace = np.minimum.accumulate(df.groupby(df.index).min()) # change if maximizing
+trace = np.minimum.accumulate(df.groupby(df.index).min())  # change if maximizing
 
 fig, ax = plt.subplots(figsize=(6, 4), dpi=150)
 ax.scatter(df.index, df, ec="k", fc="none", label="Observed")
@@ -106,7 +114,13 @@ pareto = pd.DataFrame([p[1][0] for p in pareto.values()]).sort_values(objectives
 
 fig, ax = plt.subplots(figsize=(6, 4), dpi=200)
 ax.scatter(df[objectives[0]], df[objectives[1]], fc="None", ec="k", label="Observed")
-ax.plot(pareto[objectives[0]],pareto[objectives[1]],color="#0033FF",lw=2,label="Pareto Front",)
+ax.plot(
+    pareto[objectives[0]],
+    pareto[objectives[1]],
+    color="#0033FF",
+    lw=2,
+    label="Pareto Front",
+)
 ax.set_xlabel(objectives[0])
 ax.set_ylabel(objectives[1])
 ax.legend()
